756a33f64bee612b963f6caf46f1ee6d47bcb40b
Jess Skelton <jessskelton888@gmail.com>
2024-04-01 21:15:23 -0500

    Added content to Error.jsx -- Cleaned up main.jsx & App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 // React & Apollo imports
-// import React from 'react';
 import {
   ApolloClient,
   InMemoryCache,
@@ -12,14 +11,6 @@ import { Outlet } from 'react-router-dom';
 // Component imports
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-// Temp Holding Place
-// import Hero from './pages/Hero';
-// import Schedule from './pages/Schedule';
-import Tasks from './pages/Tasks';
-// import Header from './components/Header/index';
-// import Footer from './components/Footer/index';
-
 
 // HTTP link for GraphQL operations
 const httpLink = createHttpLink({
@@ -50,12 +41,6 @@ function App() {
         <Header />
         <main className="outlet">
           <Outlet />
-
-          {/* Temp Holding Place*/}
-          {/* <Hero />
-          <Schedule /> */}
-          {/* <Tasks /> */}
-
         </main>
         <Footer />
       </div>
